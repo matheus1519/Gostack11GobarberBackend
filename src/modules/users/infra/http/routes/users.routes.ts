acacb29e5dffd7654a16f53bbc453bf0899941f5
matheus1519 <1519matheus@gmail.com>
2020-09-16 17:04:18 -0300

    Continuando o backend

---
+++ src/modules/users/infra/http/routes/users.routes.ts
@@ -0,0 +1,26 @@
+import { Router } from 'express';
+import { container } from 'tsyringe';
+import multer from 'multer';
+
+import UsersController from '../controllers/UsersController';
+import UserAvatarController from '../controllers/UserAvatarController';
+
+import uploadConfig from '@config/upload';
+
+import ensureAuthenticated from '../middlewares/ensureAuthenticated';
+
+const usersRouter = Router();
+const usersController = new UsersController();
+const userAvatarController = new UserAvatarController();
+const upload = multer(uploadConfig);
+
+usersRouter.post('/', usersController.create);
+
+usersRouter.patch(
+  '/avatar',
+  ensureAuthenticated,
+  upload.single('avatar'),
+  userAvatarController.update,
+);
+
+export default usersRouter;
